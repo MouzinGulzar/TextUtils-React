f24794917bb0605ac046189bfb508784ff4f0f4f
Mouzin Gulzar <mouzinmonis@gmail.com>
2021-09-22 22:47:49 +0530

    Final commit 🚀

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,9 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="/">
             {props.title}
-          </a>
-          {/* <Link className="navbar-brand" to="/">
-            {props.title}
-          </Link> */}
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -29,17 +26,14 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <Link className="nav-link" aria-current="page" to="/">
                   Home
-                </a>
-                {/* <Link className="nav-link" aria-current="page" to="/">
-                  Home
-                </Link> */}
+                </Link>
               </li>
               <li className="nav-item">
-                {/* <Link className="nav-link" to="/about">
+                <Link className="nav-link" to="/about">
                   {props.aboutText}
-                </Link> */}
+                </Link>
               </li>
             </ul>
             <div
